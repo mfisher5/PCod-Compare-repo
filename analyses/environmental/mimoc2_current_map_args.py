2320781656836c1d9534be1a406b1cae4c47ebe1
mfisher5@uw.edu <mfisher5@uw.edu>
2018-06-10 16:05:23 -0700

    sst map for chapt 1

---
+++ analyses/environmental/mimoc2_current_map_args.py
@@ -0,0 +1,110 @@
+############## PYTHON CODE TO SUBSET AND PLOT MIMOC DATA ################
+#
+# Written by Rosalind Echols, UW Oceanography (rechols@uw.edu) 
+#
+#
+# MF Edited 5/10/2018 to take command line arguments
+#
+###########################################################################
+
+######## arguments #########
+import argparse 
+
+parser = argparse.ArgumentParser(description="subset and plot a netCDF file from the MIMOC database.")
+
+parser.add_argument("-i", "--input", help="input file; should be a .nc.gz file from MIMOC")
+parser.add_argument("-p", "--plot", help="output plot file; should be a .jpg")
+parser.add_argument("-lat", "--latitude", help="start, end latitude. 120,135 Korean peninsula [start,end]")
+parser.add_argument("-long", "--longitude", help="start, end longitude. 30,40 Korean peninsula [start,end]")
+parser.add_argument("-d", "--depth", help="depth to retrieve data from. Coded by seq(0,x,by=5) so d1 = 0m, d2 = 5m, d2 = 10m, etc.")
+parser.add_argument("-var", "--variable", help="Variable to plot [CONSERVATIVE_TEMPERATURE / ABSOLUTE_SALINITY]")
+
+args = parser.parse_args()
+
+########## packages ############
+
+import numpy as np
+import matplotlib.pyplot as plt
+from netCDF4 import Dataset
+from mpl_toolkits.basemap import Basemap
+import gsw
+import cmocean
+
+
+########### functions ##############
+
+def import_data(file_name):
+    '''This function works regardless of the file called as long as a list of 
+    variables can be identified easily in the file.'''
+    
+    fh = Dataset(file_name, mode = 'r')
+    data = {}
+    
+    for vname in list(fh.variables):
+        data[str(vname)] = fh.variables[vname][:]
+        
+    fh.close()
+            
+    return data
+
+
+def lat_lon_indices(latitudes,longitudes,latlonrange):
+    '''This function identifies the index values for latitude and longitude
+    that correspond to the desired bounds for studying a particular region so 
+    that the data can be sliced before processing. It returns data in the form
+    [lon_min,lon_max,lat_min,lat_max] based on a latlonrange input of the
+    same value.'''
+    
+    index=np.zeros(4,dtype='int') 
+    
+    index[0] = int(next(i for i,j in enumerate(longitudes) if j == latlonrange[0]))
+    index[1] = int(next(i for i,j in enumerate(longitudes) if j == latlonrange[1]))
+    index[2] = int(next(i for i,j in enumerate(latitudes) if j == latlonrange[2]))
+    index[3] = int(next(i for i,j in enumerate(latitudes) if j == latlonrange[3]))
+                            
+    return index
+
+
+############ subset data ###############
+#MIMOC VARIABLES: ['LATITUDE', 'PRESSURE', 'POTENTIAL_TEMPERATURE', 'LONGITUDE', 'SALINITY']
+#OR:
+    #MIMOC VARIABLES: ['LATITUDE', 'PRESSURE', 'CONSERVATIVE_TEMPERATURE', 'LONGITUDE', 'ABSOLUTE_SALINITY']
+#Shape of salinity: (81, 341, 720) = depth, latitude, longitude
+
+filename=args.input
+
+#import data:
+data=import_data(filename)
+
+
+#define the latitude-longitude range you want to look at. example: [120,135,30,40] captures the Korean Peninsula area
+lon_start = int(args.longitude.split(",")[0])
+lon_end = int(args.longitude.split(",")[1])
+lat_start = int(args.latitude.split(",")[0])
+lat_end = int(args.latitude.split(",")[1])
+llrange=[lat_start,lat_end,lon_start,lon_end]  
+print llrange
+
+#find the relevant section of the data to look at based on your latitude/longitude range
+ll_index=lat_lon_indices(data['LATITUDE'],data['LONGITUDE'],llrange)
+
+############### plot data ##################
+fig=plt.figure()
+m = Basemap(llcrnrlon=data['LONGITUDE'][ll_index[0]],llcrnrlat=data['LATITUDE'][ll_index[2]],
+            urcrnrlon=data['LONGITUDE'][ll_index[1]],urcrnrlat=data['LATITUDE'][ll_index[3]],projection='mill')
+x,y=np.meshgrid(data['LONGITUDE'][ll_index[0]:ll_index[1]],data['LATITUDE'][ll_index[2]:ll_index[3]])
+
+#make a contour plot of the data you want to look at; you'll need to give it an index for the pressure, 
+#which I think is spaced out by 5 meters near the surface. Therefore, if you pick 0 for plevel, you'll get 0m,
+#1 for plevel is 5m, 2 for plevel is 10m, and so on. 
+plevel=int(args.depth)
+print('You are looking at a depth of ', data['PRESSURE'][plevel])
+m.contourf(x,y,data[args.variable][plevel,ll_index[2]:ll_index[3],ll_index[0]:ll_index[1]],10,latlon='True')
+m.drawrivers()
+m.bluemarble()
+#add lat/long lines
+m.drawmeridians(np.arange(lat_start, lat_end, 5), labels = [True] *len(np.arange(lat_start, lat_end, 4)))
+m.drawparallels(np.arange(lon_start, lon_end, 5), labels = [True]*len(np.arange(lon_start, lon_end, 4)))
+plt.colorbar()
+plt.savefig(args.plot)
+
